0ef521ff46ba57fb189d6a678e43c227b343e19a
William Wang <bn.william.w@gmail.com>
2023-04-01 17:18:26 -0400

    Remove old stats

--- src/components/PlaceCard.svelte
+++ src/components/PlaceCard.svelte
@@ -33,39 +33,6 @@
 		</div>
 	{/if}
 
-	<div class="stats shadow">
-  
-		<div class="stat">
-		  <div class="stat-figure text-primary">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-		  </div>
-		  <div class="stat-title">Rating (out of 5)</div>
-		  <div class="stat-value text-primary">{place.rating}</div>
-		  <div class="stat-desc">{Math.floor(Math.random() * 7) + 1}% higher than last month</div>
-		</div>
-		
-		<div class="stat">
-		  <div class="stat-figure text-secondary">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-		  </div>
-		  <div class="stat-title">Total Ratings</div>
-		  <div class="stat-value text-secondary">{place.ratings}</div>
-		  <div class="stat-desc">{Math.floor(Math.random() * 7) + 1}% more than last month</div>
-		</div>
-		
-		<div class="stat">
-		  <div class="stat-figure text-secondary">
-			<div class="avatar online">
-			  <div class="w-16 rounded-full">
-				<img src={place.icon} />
-			  </div>
-			</div>
-		  </div>
-		  <div class="stat-value">{Math.floor(Math.random() * 4) * 5 + 5}% Off</div>
-		  <div class="stat-title">Coupon</div>
-		  <div class="stat-desc text-secondary">{Math.floor(Math.random() * 50) + 1} coupons remaining</div>
-		</div>
-	  </div>
 	<div class="card-body overflow-hidden pt-4">
 		<h2 class="card-title line-clamp-1 text-3xl font-bold">
 			{place.name}
